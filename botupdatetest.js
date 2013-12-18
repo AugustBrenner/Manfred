c0176f8b3269939ebc18eeea490c57122edbdcd5
August Brenner <AugustBrenner@server.fake>
2013-12-17 16:29:16 -0800

    updated stateless

--- botupdatetest.js
+++ botupdatetest.js
@@ -5,13 +5,27 @@ var API                 = require('groupme').Stateless;
 
 API.Bots.update(
 	'c74e9900384b013104357e620898ea29',
-	'30ff5f0e8fa6c9df115deedecc',
-	{name : "hello", },
+	'4acf5180d9b3ae623a8f4bbc0e',
+	{name: "hello", group_id: "6566120", avatar_url: "http://i.groupme.com/1753f1f038e70131877022000aba07be"},
 	function(error, response) {
 		if (!error && response) {
 			console.log(response);		
 		} else {
-			console.log("modification error");
+			console.log("error");
 		}
 	}
 );
+
+
+/*API.Bots.create(
+	'c74e9900384b013104357e620898ea29',
+	"manfred",
+	"6566120",
+	function(error, response) {
+		if (!error && response) {
+			console.log(response);		
+		} else {
+			console.log("error");
+		}
+	}
+);*/
